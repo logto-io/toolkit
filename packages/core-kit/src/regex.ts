327b4bbf66ecacf3ecf73d402a8c7f04389b3624
Charles Zhao <charleszhao@silverhand.io>
2022-09-28 18:05:03 +0800

    refactor(core): improve redirect URI validation

--- packages/core-kit/src/regex.ts
+++ packages/core-kit/src/regex.ts
@@ -2,6 +2,7 @@ export const emailRegEx = /^\S+@\S+\.\S+$/;
 export const phoneRegEx = /^\d+$/;
 export const usernameRegEx = /^[A-Z_a-z]\w*$/;
 export const passwordRegEx = /^.{6,}$/;
-export const redirectUriRegEx = /^https?:\/\//;
+export const redirectUriRegEx = /^https?:\/\/[\d&./:=?_a-z-]+$/;
+export const nativeRedirectUriRegEx = /^[a-z][\d_a-z]*(\.[\d_a-z]+)+:\/\/[\d/_a-z-]+$/;
 export const hexColorRegEx = /^#[\da-f]{3}([\da-f]{3})?$/i;
 export const dateRegex = /^\d{4}(-\d{2}){2}/;
